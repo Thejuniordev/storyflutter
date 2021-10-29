20ac0399f1f1cd836481424de25c5fcfd47f077e
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-10-28 21:44:47 -0300

    fix: Add device in root

--- lib/src/storyflutter.dart
+++ lib/src/storyflutter.dart
@@ -4,13 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:storyflutter/src/configure_non_web.dart'
     if (dart.library.html) 'package:storyflutter/src/configure_web.dart';
 import 'package:storyflutter/src/models/app_info.dart';
+import 'package:storyflutter/src/models/device.dart';
 
 class Storyflutter extends StatefulWidget {
   final AppInfo appInfo;
 
+  final List<Device> devices;
+
   const Storyflutter({
     Key? key,
     required this.appInfo,
+    this.devices = const [
+      Apple.iPhone7,
+      Samsung.s21ultra,
+    ],
   }) : super(key: key);
   @override
   _StoryflutterState createState() => _StoryflutterState();
