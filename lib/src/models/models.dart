ba644bb99ec9553daaa267b9be8492cb57595f2b
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-10-28 22:28:01 -0300

    fix: Create services

---
+++ lib/src/models/models.dart
@@ -0,0 +1,7 @@
+export 'package:storyflutter/src/models/app_info.dart';
+export 'package:storyflutter/src/models/device.dart';
+export 'package:storyflutter/src/models/organizers/category.dart';
+export 'package:storyflutter/src/models/organizers/organizers.dart';
+export 'package:storyflutter/src/models/organizers/story.dart';
+export 'package:storyflutter/src/models/organizers/widget_element.dart';
+export 'package:storyflutter/src/models/resolution.dart';
